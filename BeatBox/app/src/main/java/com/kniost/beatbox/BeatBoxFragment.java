4d670bf5680b3e2fbdc0a2ce46c5051cfad05fdc
kniost <kniost33@gmail.com>
2017-03-13 16:40:13 +0800

    Complete chapter 19，完成第19章

--- BeatBox/app/src/main/java/com/kniost/beatbox/BeatBoxFragment.java
+++ BeatBox/app/src/main/java/com/kniost/beatbox/BeatBoxFragment.java
@@ -27,6 +27,7 @@ public class BeatBoxFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRetainInstance(true);
 
         mBeatBox = new BeatBox(getActivity());
     }
@@ -45,7 +46,14 @@ public class BeatBoxFragment extends Fragment {
         return view;
     }
 
-    private class SoundHolder extends RecyclerView.ViewHolder {
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mBeatBox.release();
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener{
         private Button mButton;
         private Sound mSound;
 
@@ -53,11 +61,17 @@ public class BeatBoxFragment extends Fragment {
             super(inflater.inflate(R.layout.list_item_sound, container, false));
 
             mButton = (Button) itemView.findViewById(R.id.list_item_sound_button);
+            mButton.setOnClickListener(this);
         }
 
         public void bindSound(Sound sound) {
             mSound = sound;
             mButton.setText(sound.getName());
+        }
+
+        @Override
+        public void onClick(View v) {
+            mBeatBox.play(mSound);
         }
     }
 
--- BeatBox/app/src/main/java/com/kniost/beatbox/Sound.java
+++ BeatBox/app/src/main/java/com/kniost/beatbox/Sound.java
@@ -7,6 +7,7 @@ package com.kniost.beatbox;
 public class Sound {
     private String mAssetPath;
     private String mName;
+    private Integer mSoundId;
 
     public Sound(String assetPath) {
         mAssetPath = assetPath;
@@ -21,5 +22,13 @@ public class Sound {
 
     public String getName() {
         return mName;
+    }
+
+    public Integer getSoundId() {
+        return mSoundId;
+    }
+
+    public void setSoundId(Integer soundId) {
+        mSoundId = soundId;
     }
 }
