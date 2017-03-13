fd445e40074aa92c9ff87d2881970b13f4556811
kniost <kniost33@gmail.com>
2017-03-13 16:43:30 +0800

    Complete chapter 22 without completing challenge，完成第22章但未完成挑战

---
+++ NerdLauncher/app/src/main/java/com/kniost/nerdlauncher/NerdLauncherActivity.java
@@ -0,0 +1,11 @@
+package com.kniost.nerdlauncher;
+
+import android.support.v4.app.Fragment;
+
+public class NerdLauncherActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return NerdLauncherFragment.newInstance();
+    }
+}
