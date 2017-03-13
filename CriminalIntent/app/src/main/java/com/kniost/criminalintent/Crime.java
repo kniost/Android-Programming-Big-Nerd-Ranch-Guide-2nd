0917ea242f26e72902e574860432fa7eb0bfaff8
kniost <kniost33@gmail.com>
2017-03-13 15:50:18 +0800

    Complete chapter 7,完成第七章

---
+++ CriminalIntent/app/src/main/java/com/kniost/criminalintent/Crime.java
@@ -0,0 +1,28 @@
+package com.kniost.criminalintent;
+
+import java.util.UUID;
+
+/**
+ * Created by kniost on 2016/10/26.
+ */
+
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+
+    public Crime() {
+        mId = UUID.randomUUID();
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
---
+++ CriminalIntent/app/src/main/java/com/kniost/criminalintent/CrimeFragment.java
@@ -0,0 +1,50 @@
+package com.kniost.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+/**
+ * Created by kniost on 2016/10/26.
+ */
+
+public class CrimeFragment extends Fragment {
+    private Crime mCrime;
+    private EditText mTitleField;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mCrime = new Crime();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+        mTitleField = (EditText) v.findViewById(R.id.crime_title);
+        mTitleField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //Do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                mCrime.setTitle(charSequence.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //Do nothing
+            }
+        });
+        return v;
+    }
+}
