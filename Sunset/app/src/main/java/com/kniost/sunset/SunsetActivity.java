45952a421e4c7c73d7fcffe394876d645c03c168
kniost <kniost33@gmail.com>
2017-03-15 17:55:38 +0800

    Complete chapter 30 without finishing challenges，完成第30章但未完成挑战

---
+++ Sunset/app/src/main/java/com/kniost/sunset/SunsetActivity.java
@@ -0,0 +1,11 @@
+package com.kniost.sunset;
+
+import android.support.v4.app.Fragment;
+
+public class SunsetActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return SunsetFragment.newInstance();
+    }
+}
